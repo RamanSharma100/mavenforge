bec9d9764edd374212cf52433f80a5f6c26fd749
Raman Sharma <techraman100@gmail.com>
2025-01-05 19:24:20 +0530

    Added Session in Authentication

--- src/main/java/com/mavenforge/Controllers/Auth.java
+++ src/main/java/com/mavenforge/Controllers/Auth.java
@@ -1,13 +1,17 @@
-package com.mavenforge.Services;
+package com.mavenforge.Controllers;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 
 import com.mavenforge.Application;
 import com.mavenforge.Crypto.Hashing;
 import com.mavenforge.Database.Database;
+import com.mavenforge.Services.Cookie;
+import com.mavenforge.Services.HttpSession;
+import com.mavenforge.Utils.Environment;
 
-public class Auth {
+public class Auth extends Controller {
     public static boolean attempt(String email, String password) {
         Database db = Application.database;
         String databaseType = db.databaseType.toString();
@@ -56,6 +60,14 @@ public class Auth {
 
                         Application.database.mySQLDatabase.update("users", "last_login", values, "email = " + email);
 
+                        // String session = Cookie.get("SESSION_ID").getValue();
+                        String APP_NAME = Environment.get("APP_NAME");
+                        System.out.println("APP_NAME " + APP_NAME);
+
+                        Cookie.set(APP_NAME.substring(0, 3) + "_atoken_mfg", token).setExpiry(
+                                Instant.ofEpochMilli(60 * 60 * 24 * 30));
+                        Cookie.set(APP_NAME.substring(0, 3) + "_usr_mfg", user.toString());
+
                         return true;
                     }
                 } else {
@@ -69,6 +81,13 @@ public class Auth {
 
                     Application.database.mySQLDatabase.insert(tokenData);
 
+                    String APP_NAME = Environment.get("APP_NAME");
+                    System.out.println("APP_NAME " + APP_NAME);
+
+                    Cookie.set(APP_NAME.substring(0, 3) + "_atoken_mfg", token).setExpiry(
+                            Instant.ofEpochMilli(60 * 60 * 24 * 30));
+                    Cookie.set(APP_NAME.substring(0, 3) + "_usr_mfg", user.toString());
+
                     return true;
                 }
 
@@ -81,6 +100,13 @@ public class Auth {
                 tokenData.put("expires_at", System.currentTimeMillis() + (60 * 60 * 24 * 30));
 
                 Application.database.mySQLDatabase.insert(tokenData);
+
+                String APP_NAME = Environment.get("APP_NAME");
+                System.out.println("APP_NAME " + APP_NAME);
+
+                Cookie.set(APP_NAME.substring(0, 3) + "_atoken_mfg", token).setExpiry(
+                        Instant.ofEpochMilli(60 * 60 * 24 * 30));
+                Cookie.set(APP_NAME.substring(0, 3) + "_usr_mfg", user.toString());
 
                 return true;
 
